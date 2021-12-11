5135e8182ee22dbdaafb5942524b38d2b8a5a51e
Yoinnkkk <wojpaw2007@gmail.com>
2021-12-11 15:02:08 +0000

    editted formatter

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ var ValueLoader = window.setInterval( function() {
 function formatter(number) {
     let exponent = Math.floor(Math.log10(number))
     let mantissa = number / Math.pow(10, exponent)
-    if (exponent < 3) return number.toFixed(1)
+    if (exponent < 6) return number.toFixed(1)
     return mantissa.toFixed(2) + "e" + exponent
 }
 
