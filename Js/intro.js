f62a849724633aebf12b0ec1fd33f51c038cf5cd
Yoinnkkk <wojpaw2007@gmail.com>
2022-08-18 09:04:38 +0100

    commit

--- Js/intro.js
+++ Js/intro.js
@@ -9,23 +9,33 @@
 // Text on screen
 
 function intro() {
+    // Rain animation start
     const body = document.body
     console.log("Working!");
     animcanvas = document.createElement("div");
     animcanvas.classList.add("animcanvas");
     body.appendChild(animcanvas)
-    i = 0
-    while (i < 100) {
-        div = document.createElement("div");
-        div.classList.add("blooddrop");
-        animcanvas.appendChild(div);
-        img = document.createElement("img");
-        img.src = "Images/bloodDrop.png";
-        div.appendChild(img);
-
-    }
+    const timer = setInterval(createDrop, 50)
+    setTimeout(function () {
+        clearInterval(timer)
+        animcanvas.style.display = "none"
+        const menu = document.getElementById("suMenus")
+        menu.
+    }, 5000)
 }
 
+function createDrop() {
+    const rain = document.createElement("div");
+    rain.classList.add("blooddrop");
+    rain.style.left = `${Math.random() * 100}vw`
+    rain.style.animationDuration = `${Math.random() * 1 + 2}s`
+    animcanvas.appendChild(rain);
+    img = document.createElement("img");
+    img.src = "Images/bloodDrop.png";
+    rain.appendChild(img);
+
+    setTimeout(() => rain.remove(), 3000)
+}
 
 // Calls intro if first time
 if (localStorage.getItem("AltarSave") == null) intro()
